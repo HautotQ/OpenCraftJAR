e8da439d5308811a462dfc07240e3aff5bfa074c
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-10-11 14:01:47 +0200

    Update ContentView.java

--- ContentView.java
+++ ContentView.java
@@ -36,52 +36,9 @@ public class ContentView extends JPanel {
             setCenterPanel(view);
         });
 
-        JMenuItem questionsExample = new JMenuItem("Exemples de Questions");
-        questionsExample.addActionListener(e -> {
-            Question[] temp = {
-                new Question("1+1", "2"),
-                new Question("1+2", "3"),
-                new Question("1+3", "4"),
-                new Question("1+4", "5"),
-                new Question("1+5", "6"),
-                new Question("1+6", "7")
-            };
-            JPanel example = new QuestionsExample(temp);
-            setCenterPanel(example);
-        });
-
-        JMenuItem editQuestions = new JMenuItem("Éditeur de fichiers clist/txt");
-        editQuestions.addActionListener(e -> {
-            setCenterPanel(new JLabel("Chargement de l’éditeur...", SwingConstants.CENTER));
-            SwingUtilities.invokeLater(() -> {
-                JPanel editor = new FileEdit();
-                setCenterPanel(editor);
-            });
-        });
-
         craftion.add(addQuestion);
         craftion.add(playQuestions);
         craftion.add(viewQuestions);
-        craftion.addSeparator();
-        craftion.add(editQuestions);
-
-        // Menu "Commandes"
-        JMenu cmds = new JMenu("Commandes");
-        JMenuItem commands = new JMenuItem("Commandes");
-        commands.addActionListener(e -> {
-            JPanel command = new CommandView();
-            setCenterPanel(command);
-        });
-
-        JMenuItem cmdsList = new JMenuItem("Liste de commandes");
-        cmdsList.addActionListener(e -> {
-            String[] cmdsTable = { "/say Hello World", "/clear" };
-            JPanel commandsList = new CommandsList(cmdsTable);
-            setCenterPanel(commandsList);
-        });
-
-        cmds.add(commands);
-        cmds.add(cmdsList);
 
         // Menu "À propos"
         JMenu about = new JMenu("À propos");
@@ -110,7 +67,6 @@ public class ContentView extends JPanel {
 
         // Barre de menus complète
         menuBar.add(craftion);
-        menuBar.add(cmds);
         menuBar.add(about);
 
         // Zone centrale
